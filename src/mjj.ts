5db2d5e5ba39283bcf6e439d409bf74bdc2fbd4e
caoyufei <atcaoyufei@gmail.com>
2023-07-15 17:37:05 +0800

    init

---
+++ src/mjj.ts
@@ -0,0 +1,91 @@
+import * as cheerio from 'cheerio';
+
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms * 1000));
+}
+
+const base_api = 'https://share.cjy.me'
+const login_url = `${base_api}/mjj6/`
+const node_url = `${base_api}/nodeinfo/`
+
+const toClash = (item) => {
+    const network = item['net']
+    let data = {
+        "name": item['ps'],
+        "server": item['add'],
+        "port": item['port'],
+        "type": "vmess",
+        "uuid": item['id'],
+        "alterId": item['aid'],
+        "cipher": "auto",
+        "tls": item['tls'] === undefined || item['tls'] === 'none',
+        "skip-cert-verify": true,
+    }
+    if (network === 'ws') {
+        let host = item['host']
+        if (!host || host === 'none') {
+            host = item['add']
+        }
+        data['network'] = network
+        data['ws-opts'] = {
+            "path": item['path'],
+            "headers": {
+                "Host": host
+            }
+        }
+    }
+    return data
+}
+
+export const get_nodes = async (cookie) => {
+    let response = await fetch(node_url, {headers: {Cookie: cookie}});
+    let html = await response.text()
+    const $ = cheerio.load(html)
+    const items = $('div.node-cell')
+    let clashData = []
+    for (let i = 0; i <= items.length; i++) {
+        let status = $(items[i]).find('.card-header-title span').text()
+        if (status === '主机故障') {
+            continue
+        }
+        let vmess = $(items[i]).find('.modal a').attr('href')
+        if (vmess === undefined || typeof vmess === 'undefined') {
+            continue
+        }
+        try {
+            vmess = atob(vmess.replace('vmess://', ''))
+            clashData.push(toClash(JSON.parse(vmess)))
+        } catch (e) {
+            console.log(e)
+        }
+    }
+    return clashData
+}
+
+export const login = async (username, password) => {
+    let response = await fetch(login_url)
+    let cookies = response.headers.getSetCookie()
+
+    const html = await response.text()
+    const token = html.match('name="csrfmiddlewaretoken" value="([^"]+)"')
+    if (token === null) {
+        return null
+    }
+
+    let body = new FormData()
+    body.append('csrfmiddlewaretoken', token[1])
+    body.append('username', username)
+    body.append('password', password)
+    let options = {
+        body: body,
+        method: 'POST',
+        redirect: 'manual',
+        headers: {Cookie: cookies[0], Referer: login_url}
+    }
+    response = await fetch(login_url, options);
+    let location = response.headers.get('location')
+    if (location && location.includes('userinfo')) {
+        return response.headers.getSetCookie().join(';')
+    }
+    return null
+}
---
+++ src/worker.ts
@@ -0,0 +1,200 @@
+import {Router} from 'itty-router';
+import {DefaultClashYml, DevClashRule} from './clash';
+import {login, get_nodes} from './mjj';
+
+import YAML from 'yaml'
+
+class Subscribe {
+
+    private _request: Request;
+    private _env: Env
+    private _kv: KVNamespace;
+    private testUrl = 'http://www.gstatic.com/generate_204'
+    private subscribeName: string = 'subscribe'
+    private subscribeNodeName: string = 'subscribe_node'
+    private mjjCookieName: string = 'share_cjy_cookie'
+    private mjjCookie
+    private jsonHeader = {
+        headers: {
+            "content-type": "application/json;charset=UTF-8",
+        }
+    }
+    private fetchUserAgent = {'User-Agent': 'ClashMetaForAndroid/2.7.1.Meta-Alpha (Prefer ClashMeta Format)'}
+    private providerTemplate = {
+        'interval': 7200, 'url': '', 'type': 'http', 'path': '',
+        'health-check': {'enable': true, 'interval': 7200, 'url': this.testUrl}
+    }
+
+    async init(env: Env, request: any) {
+        this._env = env
+        this._request = request
+        this._kv = env.SUBSCRIBE
+
+        let cookie = await this._kv.get(this.mjjCookieName)
+        if (cookie === null) {
+            cookie = await login(env.SECRET_USERNAME, env.SECRET_PASSWORD)
+            if (cookie !== null) {
+                console.log(`Logged in successfully! \nCookie: ${cookie}`)
+                await this._kv.put(this.mjjCookieName, cookie, {expirationTtl: 3600 * 22})
+                return
+            }
+            console.log(`[${env.SECRET_USERNAME}] Login Fail.`)
+        }
+        if (cookie !== null) {
+            this.mjjCookie = cookie
+        }
+    }
+
+    trim(str, char) {
+        const reg = new RegExp(`^${char}+|${char}+$`, 'g');
+        return str.replace(reg, '');
+    }
+
+    async save_subscribe(request) {
+        const formData = await request.formData();
+        let subscribeData = {};
+        formData.forEach(function (value, key) {
+            subscribeData[key] = value;
+        });
+        const result = JSON.stringify(subscribeData)
+        await this._kv.put(this.subscribeName, result)
+        return new Response(result, this.jsonHeader);
+    }
+
+    async get_subscribe(is_dev: boolean) {
+        const value = await this._kv.get(this.subscribeName)
+        if (value === null) {
+            return new Response('请先添加订阅', {status: 404});
+        }
+        const subscribes = JSON.parse(value)
+        const userAgent = this._request.headers.get('User-Agent')
+        let code = YAML.parse(DefaultClashYml)
+        let _proxiesNames = []
+
+        code['proxy-providers'] = {}
+        code['proxy-groups'] = [
+            {'name': '全局选择', 'type': 'select', 'proxies': ['故障转移', '自动选择', '机场节点']},
+            {'name': '机场节点', 'type': 'select', 'proxies': []},
+            {'name': '故障转移', 'type': 'fallback', 'proxies': [], 'interval': 7200, 'url': this.testUrl},
+            {'name': '自动选择', 'type': 'url-test', 'use': [], 'interval': 7200, 'url': this.testUrl},
+        ]
+        let j = 2
+        for (let i in subscribes) {
+            if (userAgent && !userAgent.includes('meta') && i === 'BEST') {
+                continue
+            }
+            const url = encodeURIComponent(subscribes[i])
+            let provider = JSON.parse(JSON.stringify(this.providerTemplate))
+            provider['url'] = `${this.trim(this._request.url, '/')}/convert?url=${url}&name=${i}`
+            provider['path'] = `provider1/${i}.yaml`
+            _proxiesNames.push(i)
+            code['proxy-providers'][i] = provider
+            code['proxy-groups'].splice(j, 0, {'name': i, 'type': 'select', 'use': [i]})
+            j += 1
+        }
+
+        const n = code['proxy-groups'].length
+        code['proxy-groups'][1]['proxies'] = _proxiesNames
+        code['proxy-groups'][n - 2]['proxies'] = _proxiesNames
+        code['proxy-groups'][n - 1]['use'] = _proxiesNames
+
+        if (is_dev) {
+            for (let rule in DevClashRule) {
+                code['rules'].splice(0, 0, rule)
+            }
+        }
+        return new Response(YAML.stringify(code));
+    }
+
+    async convert(url, name) {
+        const subscribeNodeKey = `${this.subscribeNodeName}_${name}`
+        const value = await this._kv.get(subscribeNodeKey)
+        let data = []
+        if (value !== null && value !== '[]') {
+            data = JSON.parse(value)
+        } else {
+            if (url.includes('share.cjy.me')) {
+                data = await get_nodes(this.mjjCookie)
+            } else {
+                data = await this.update_subscribe_by_url(url)
+            }
+            console.log(`[${name}] 节点数量: ${data.length}`)
+            await this._kv.put(subscribeNodeKey, JSON.stringify(data))
+        }
+        return this.do_response(YAML.stringify({proxies: data}), {'headers': this.jsonHeader})
+    }
+
+    async update_subscribe() {
+        const value = await this._kv.get(this.subscribeName)
+        if (value === null) {
+            return
+        }
+        const subscribes = JSON.parse(value)
+        for (let i in subscribes) {
+            const url = subscribes[i]
+            let data = []
+            console.log(`Update subscribe: ${i}`)
+            if (url.includes('share.cjy.me')) {
+                data = await get_nodes(this.mjjCookie)
+            } else {
+                data = await this.update_subscribe_by_url(url)
+            }
+            await this._kv.put(`${this.subscribeNodeName}_${i}`, JSON.stringify(data))
+            console.log(`[${i}] 节点数量: ${data.length}`)
+        }
+    }
+
+    async update_subscribe_by_url(url: string) {
+        const response = await fetch(url, {headers: this.fetchUserAgent})
+        let html = await response.text()
+        if (html.includes('proxies')) {
+            const data = YAML.parse(html)
+            return data['proxies']
+        }
+        return []
+    }
+
+    do_response(data, init = {}) {
+        return new Response(data, init)
+    }
+}
+
+const subscribe = new Subscribe()
+const router = Router();
+
+router.post('/save', async (request) => {
+    return await subscribe.save_subscribe(request)
+});
+
+router.get('/convert', async (request) => {
+    const query = request.query
+    return await subscribe.convert(query['url'], query['name'])
+});
+
+router.get('/update', async (request) => {
+    await subscribe.update_subscribe()
+    return subscribe.do_response('Update successfully.')
+});
+
+router.get('/', async (request) => {
+    return await subscribe.get_subscribe(request.query['dev'] !== undefined)
+});
+
+router.all('*', () => new Response('404, not found!', {status: 404}));
+
+export default {
+    async scheduled(event: ScheduledEvent, env: Env, ctx: ExecutionContext): Promise<void> {
+        await subscribe.init(env, event)
+        await subscribe.update_subscribe()
+    },
+
+    async fetch(request: Request, env: Env, ctx: ExecutionContext): Promise<Response> {
+        await subscribe.init(env, request)
+        try {
+            return router.handle(request);
+        } catch (error) {
+            return new Response(error.message);
+        }
+    }
+};
+
